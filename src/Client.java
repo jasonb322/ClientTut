261d19d66159ecd0ca1c7f9eb58d0cdae6d5a675
jasonb322 <bahrcreative@gmail.com>
2026-05-08 20:22:00 -0400

    Create client class

---
+++ src/Client.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Client {
+    public static void main(String[] args) {
+
+        Socket socket = null;
+
+        // sequence of data
+        // improve efficiency with buffer
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter  outputStreamWriter = null;
+        BufferedReader bufferReaderClass = null;
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            // communication line
+            socket = new Socket("localhost", 1234);
+
+            // bridge to characters
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+
+            bufferedReader = new BufferedReader(inputStreamReader);
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            Scannner scannner = new Scanner(System.in);
+
+            while (true)
+            {
+                String msgToSend = scanner.nextLine();
+                bufferedWriter.write(msgToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                System.out.println("Server: " + bufferedReader.readLine();
+
+                if(msgToSend.equalsIgnoreCase("bye")) break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(socket != null)
+                    socket.close();
+                if(inputStreamReader != null)
+                    inputStreamReader.close();
+                if(outputStreamWriter != null)
+                    outputStreamWriter.close();
+                if(bufferedReader != null)
+                    bufferedReader.close();
+                if(bufferedWriter != null)
+                    bufferedWriter.close();
+
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        
+    }
+}
